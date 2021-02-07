38c1c6be2886f6de4dc25d65c9d3acacf3e65eb1
victor-vas <victorvasconcelos88@gmail.com>
2021-02-07 19:21:36 -0300

    updated views UseState and UseEffect

--- src/views/examples/UseEffect.tsx
+++ src/views/examples/UseEffect.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageTitle from '../../components/PageTitle';
 
+function calcFatorial(n: number): number {
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
+}
+
 const UseEffect = () => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(() => {
+    setFatorial(calcFatorial(number));
+  }, [number]);
+
   return (
     <div className="UseEffect">
       <PageTitle
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">
+            {fatorial === -1 ? 'Não Existe' : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(Number(e.target.value))}
+        />
+      </div>
     </div>
   );
 };
--- src/views/examples/UseState.tsx
+++ src/views/examples/UseState.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageTitle from '../../components/PageTitle';
+import SectionTitle from '../../components/SectionTitle';
 
 const UseState = () => {
+  const [count, setContent] = useState(0);
+  const [name, setName] = useState('');
+
   return (
     <div className="UseState">
       <PageTitle
         title="Hook UseState"
         subtitle="Estado em componentes funcionais!"
       />
+      <SectionTitle title="Exercício #1" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => setContent((current) => current - 10)}
+          >
+            -10
+          </button>
+          <button className="btn" onClick={() => setContent(count - 1)}>
+            -
+          </button>
+          <button className="btn" onClick={() => setContent(0)}>
+            0
+          </button>
+          <button className="btn" onClick={() => setContent(count + 1)}>
+            +
+          </button>
+          <button
+            className="btn"
+            onClick={() => setContent((current) => current + 10)}
+          >
+            +10
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício #2" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <span className="text">{name}</span>
     </div>
   );
 };
