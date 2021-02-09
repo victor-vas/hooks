c7a9b81a2c20cf5949199d406132396cc2f87bfb
victor-vas <victorvasconcelos88@gmail.com>
2021-02-09 10:40:49 -0300

    updated views UseMemo and UseCallback

--- src/views/examples/UseCallback.tsx
+++ src/views/examples/UseCallback.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PageTitle from '../../components/PageTitle';
 
 const UseCallback = () => {
+  const [count, setCount] = useState(0);
+
+  const inc = useCallback(
+    (delta: number) => {
+      setCount((current) => current + delta);
+    },
+    [setCount],
+  );
+
   return (
     <div className="UseCallback">
       <PageTitle
         title="Hook UseCallback"
         subtitle="Retorna uma função memoizada!"
       />
+      <div className="center">
+        <span className="text">{count}</span>
+        <UseCallbackButtons inc={inc} />
+      </div>
     </div>
   );
 };
+
+interface UseCallbackButtons {
+  inc: (delta: number) => void;
+}
+
+// eslint-disable-next-line react/display-name
+const UseCallbackButtons = React.memo(({ inc }: UseCallbackButtons) => {
+  console.log('render...');
+  return (
+    <div>
+      <button className="btn" onClick={() => inc(6)}>
+        +6
+      </button>
+      <button className="btn" onClick={() => inc(12)}>
+        +12
+      </button>
+      <button className="btn" onClick={() => inc(18)}>
+        +18
+      </button>
+    </div>
+  );
+});
 
 export default UseCallback;
--- src/views/examples/UseMemo.tsx
+++ src/views/examples/UseMemo.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+// import React, { useEffect,  useState } from 'react';
 import PageTitle from '../../components/PageTitle';
 
 const UseMemo = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [number3, setNumber3] = useState(0);
+
+  // const [result, setResult] = useState(0);
+
+  const sum = (number1: number, number2: number): number => {
+    const future = Date.now() + 2000;
+    while (Date.now() < future) {
+      //
+    }
+    return number1 + number2;
+  };
+
+  // Estratégia alternativa ao useMemo
+  // useEffect(() => {
+  //   setResult(sum(number1, number2));
+  // }, [number1, number2]);
+
+  const result = useMemo(() => sum(number1, number2), [number1, number2]);
+
   return (
     <div className="UseMemo">
       <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
+      <div className="center">
+        <input
+          type="number"
+          className="input"
+          value={number1}
+          onChange={(e) => setNumber1(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={number2}
+          onChange={(e) => setNumber2(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={number3}
+          onChange={(e) => setNumber3(parseInt(e.target.value))}
+        />
+        <span className="text">{result}</span>
+      </div>
     </div>
   );
 };
